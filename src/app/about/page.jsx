5bf2f202bbc9e632bd5e26f1b99fe9e245784047
Jayedjb765 <jayedjb76@gmail.com>
2025-07-22 22:03:14 +0600

    image optimization

---
+++ src/app/about/page.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+export const metadata = {
+  title: "about us",
+  description: "Welcome to about page",
+};
+
+const AboutPage = () => {
+  return (
+    <div>
+      <h1>This is AboutPage</h1>
+    </div>
+  );
+};
+
+export default AboutPage;
--- src/app/layout.js
+++ src/app/layout.js
@@ -1,13 +1,14 @@
-import { Geist, Geist_Mono } from "next/font/google";
+// import { Geist, Geist_Mono } from "next/font/google";
+import { Roboto } from 'next/font/google'
 import "./globals.css";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
+// const geistSans = Geist({
+//   variable: "--font-geist-sans",
+//   subsets: ["latin"],
+// });
 
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
+const roboto = Roboto({
+  weight: '400',
   subsets: ["latin"],
 });
 
@@ -20,7 +21,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={roboto.className}
       >
         {children}
       </body>
