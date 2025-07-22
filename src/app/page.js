69d731b3a1787a064252377bdf66473b088722f6
Jayedjb765 <jayedjb76@gmail.com>
2025-07-22 22:51:30 +0600

    data fetching

--- src/app/page.js
+++ src/app/page.js
@@ -1,6 +1,9 @@
 import React from 'react';
 
-const Home = () => {
+const Home = async () => {
+  const res = await fetch('http://localhost:5000/products')
+  const products = await res.json()
+  console.log(products);
   return (
     <div>
       <h1 className='text-center font-extrabold text-7xl'>Welcome to Home Page</h1>
