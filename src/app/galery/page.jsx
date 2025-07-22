c177641a117a7a0dd270a63fd3d87e75e4df8d4d
Jayedjb765 <jayedjb76@gmail.com>
2025-07-22 12:31:16 +0600

    image optimization

---
+++ src/app/galery/page.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const ImageOptimization = () => {
+  return (
+    <div>
+      <h1 className="text-center">Regular Image</h1>
+      <img
+        src="https://i.ibb.co/XpPFyFh/Netflix.jpg"
+        alt=""
+        className="mx-auto"
+      />
+    </div>
+  );
+};
+
+export default ImageOptimization;
