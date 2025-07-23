a56741d143337f6a4672a860b8cc89108a69b121
Jayedjb765 <jayedjb76@gmail.com>
2025-07-23 23:44:10 +0600

    data fetching

--- src/app/products/error.jsx
+++ src/app/products/error.jsx
@@ -4,6 +4,14 @@ const ErrorPage = ({ error, reset }) => {
   return (
     <div>
       <h1 className="text-red-700 text-center text-xl">SomeThing went wron</h1>
+      <div className="text-center">
+        <button
+          onClick={() => reset()}
+          className="bg-black text-white py-2 px-5 rounded-lg  transition-all "
+        >
+          Try Again
+        </button>
+      </div>
     </div>
   );
 };
