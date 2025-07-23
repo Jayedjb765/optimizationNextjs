5defeb299fa95e96e0901e3fe8a84d7784be1338
Jayedjb765 <jayedjb76@gmail.com>
2025-07-23 11:07:45 +0600

    data fetching

--- src/app/page.js
+++ src/app/page.js
@@ -1,12 +1,18 @@
+import Product from '@/components/products/Product';
 import React from 'react';
 
 const Home = async () => {
-  const res = await fetch('http://localhost:5000/products')
+  const res = await fetch('http://localhost:5000/products', {
+    next: {
+      revalidate: 5,
+    }
+  })
   const products = await res.json()
-  console.log(products);
+
   return (
     <div>
       <h1 className='text-center font-extrabold text-7xl'>Welcome to Home Page</h1>
+      <div className='grid grid-cols-3 gap-7 w-[90%] mx-auto'>{products.map(product => <Product key={product.id} product={product}></Product>)}</div>
     </div>
   );
 };
